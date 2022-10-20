059455799ca2d6ff4236901809a1d09f80e1e5d9
alfmunny <alfmunny@gmail.com>
2022-10-20 07:12:12 +0200

    updated main

--- main.cpp
+++ main.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
 #include <rpc/client.h>
 
-int main() {
-    rpc::client client("localhost", 8080);
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        std::cout << "Usage: Provide host and port of the primary node" << std::endl;
+    }
+
+    rpc::client client(argv[1], atoi(argv[2]));
+
+    std::cout << "Connecting to " << argv[1] << ":" << argv[2] << std::endl;
+
+    std::cout << "Inserting {alice, alice@gmail.com}" << std::endl;
+    client.call("PUT", "alice", "alice@gmail.com");
+    std::cout << "Inserting {bob, bob@gmail.com}" << std::endl;
+    client.call("PUT", "bob", "bob@gmail.com");
+
+    std::cout << "Get bob" << std::endl;
     std::cout << client.call("GET", "bob").as<std::pair<bool, std::string>>().second << std::endl;
+    std::cout << "Get alice" << std::endl;
     std::cout << client.call("GET", "alice").as<std::pair<bool, std::string>>().second << std::endl;
-    std::cout << client.call("GET", "haha").as<std::pair<bool, std::string>>().second << std::endl;
+    std::cout << "Get nothing" << std::endl;
+    std::cout << client.call("GET", "nothing").as<std::pair<bool, std::string>>().second << std::endl;
 
-    client.call("PUT", "wei", "wei@gmail.com");
+    std::cout << "Delete bob" << std::endl;
+    client.call("DEL", "bob");
 
-    std::cout << client.call("GET", "wei").as<std::pair<bool, std::string>>().second << std::endl;
-
-    client.call("DEL", "wei");
-
-    std::cout << client.call("GET", "wei").as<std::pair<bool, std::string>>().second << std::endl;
+    std::cout << "Get bob: empty" << std::endl;
+    std::cout << client.call("GET", "bob").as<std::pair<bool, std::string>>().second << std::endl;
 
     return 0;
 }
